b2cef1fdebbd6ca60edf8fe1667bb76a31e7124b
Gilblasse <Gilblasse@gmail.com>
2020-08-28 16:32:07 -0400

    added correct api endpoint

--- src/constants/APIs.js
+++ src/constants/APIs.js
@@ -2,7 +2,7 @@ const baseURL = "https://api.github.com"
 const userDataAPI = owner => `${baseURL}/users/${owner}`
 const repoAPI = (owner, repo) =>`${baseURL}/repos/${owner}/${repo}`
 const followersAPI = owner =>`${baseURL}/users/${owner}/followers`
-const repoPackagesAPI = `https://git.heroku.com/find-repo-packages.git/api/v1/repo-packages-info`
+const repoPackagesAPI = "https://find-repo-packages.herokuapp.com/api/v1/repo-packages-info"
 
 
 export{
